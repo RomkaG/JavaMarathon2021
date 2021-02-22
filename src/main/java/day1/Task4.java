6df01169cb0e790c579bf9692cdcb5067c27c1c7
RomkaG <bossroman5@gmail.com>
2021-02-22 21:57:38 +0300

    fixed tasks 1 - 5 Day_1

--- src/main/java/day1/Task4.java
+++ src/main/java/day1/Task4.java
@@ -4,7 +4,7 @@ public class Task4 {
     public static void main(String[] args) {
      int year = 1980;
      while (year <= 2020){
-         System.out.println("Олмпиада " + year + " года");
+         System.out.println("Олимпиада " + year + " года");
          year = year + 4;
      }
     }
