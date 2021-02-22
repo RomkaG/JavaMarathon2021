0d52a22e4c7e0a4c0d22ce24f3462952f65ac2b8
RomkaG <bossroman5@gmail.com>
2021-02-22 21:32:51 +0300

    Add Task6 solution

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,13 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
+    int m;// результат произведения k*i
+    int k = 5;
+    for (int i = 1; i <= 9; i ++){
+        m = i * k;
+        System.out.println(i + "*" + k + "=" + m);
 
+      }
     }
+
 }
