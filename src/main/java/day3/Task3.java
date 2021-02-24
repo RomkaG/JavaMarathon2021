da87f0a2da019e03efc6204a781d97215a20ccc2
RomkaG <bossroman5@gmail.com>
2021-02-24 23:32:49 +0300

    Add solutions tasks 1-3 Day_3

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        double a,b;
+        for (int i = 1; i <=5; i++){
+            a = scanner.nextDouble();
+            b = scanner.nextDouble();
+            if (b == 0){
+                System.out.println("Деление на 0");
+                continue;
+            }
+            a = a/b;
+            System.out.println(a);
+        }
     }
 }
